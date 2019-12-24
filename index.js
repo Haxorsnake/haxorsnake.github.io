ff86c0658cd6b5c0a46de56dadf2b499092e87aa
Haxorsnake <optic2010@live.no>
2019-12-24 03:42:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function test() {
     var imageHolder = document.querySelector(".images");
     let imgEle = document.createElement("img");
 
-    imgEle.src = "https://pbs.twimg.com/profile_images/1148502291692965889/rdZ5NNWh_400x400.png";
-    imageHolder.appendChild(imgEle);
+    if (imageHolder.children.length == 10) {
+        imgEle.src = "https://cdn.pixabay.com/photo/2017/07/27/12/12/stop-2545121_960_720.png";
+        imageHolder.appendChild(imgEle);
+    }else if (imageHolder.children.length < 10) {
+        imgEle.src = "https://pbs.twimg.com/profile_images/1148502291692965889/rdZ5NNWh_400x400.png";
+        imageHolder.appendChild(imgEle);
+        console.log(":D");
+    }
 }
