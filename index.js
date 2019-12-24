d0bbc578a225b9094e3a37be52019a28ec963a75
Haxorsnake <optic2010@live.no>
2019-12-24 03:35:44 +0100

    fixed stuff

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-var imageHolder = document.querySelector(".imageHolder");
-
 function test() {
+    var imageHolder = document.querySelector(".images");
     let imgEle = document.createElement("img");
 
     imgEle.src = "https://pbs.twimg.com/profile_images/1148502291692965889/rdZ5NNWh_400x400.png";
