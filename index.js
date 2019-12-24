94046f907a0445b4a0a1a9b22bd481c41b1bca04
Haxorsnake <optic2010@live.no>
2019-12-24 03:52:41 +0100

    new site

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function test() {
-    var imageHolder = document.querySelector(".images");
-    let imgEle = document.createElement("img");
+document.onkeydown = checkKey;
 
-    if (imageHolder.children.length == 10) {
-        imgEle.src = "https://cdn.pixabay.com/photo/2017/07/27/12/12/stop-2545121_960_720.png";
-        imageHolder.appendChild(imgEle);
-    }else if (imageHolder.children.length < 10) {
-        imgEle.src = "https://pbs.twimg.com/profile_images/1148502291692965889/rdZ5NNWh_400x400.png";
-        imageHolder.appendChild(imgEle);
-        console.log(":D");
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '39') {
+        console.log("Testing")
+
+    }else if (e.keyCode == '37'){
+        console.log("Testing")
+
     }
+
 }
